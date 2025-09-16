d7e463d739d1681b42dea6780edc0ebfdef62b0a
Samjoash9 <samjoashantonio@gmail.com>
2025-09-16 21:33:18 +0800

    working na ang navigation sa login and register

--- resources/js/components/common/HeaderButton.tsx
+++ resources/js/components/common/HeaderButton.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { router } from '@inertiajs/react';
 import '../../../css/components/common/HeaderButton.css';
 
-const HeaderButton = ({ ButtonTitle = "Sign Up" }) => {
-    return (
-        <button className="header-button">{ButtonTitle}</button>
-    );
+interface HeaderButtonProps {
+    ButtonTitle?: string;
 }
+
+const HeaderButton = ({ ButtonTitle = "Sign Up" }: HeaderButtonProps) => {
+    const handleClick = () => {
+        const path = ButtonTitle.toLowerCase() === 'sign up' ? '/register' : '/login';
+        console.log(path);
+        router.visit(path);
+    };
+
+    return (
+        <button onClick={handleClick} className="header-button">
+            {ButtonTitle}
+        </button>
+    );
+};
 
 export default HeaderButton;
